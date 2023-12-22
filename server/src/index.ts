8f586ba9dfaf856a52a08be8bf0ad3ce53d512a9
Andrew <github@seventh.space>
2023-12-22 00:33:12 +0000

    chore: replace Socket.IO server with uWebSocket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,36 @@
-// import { app } from "./express"
-import { start } from "./socketio"
+import uws from "uWebSockets.js"
 
-start()
+const PORT = 3010
+
+const app = uws.App({})
+
+interface UserData {
+    id: string
+}
+
+app.ws<UserData>("/ws/*", {
+    open(ws) {
+        const userData = ws.getUserData()
+        userData.id = crypto.randomUUID()
+        console.log(`Opened ${userData.id}`)
+    },
+    message(ws, message, isBinary) {
+        ws.send(message, isBinary, true)
+    },
+    close(ws, code, message) {
+        const userData = ws.getUserData()
+        console.log(`Closed ${userData.id}`)
+    },
+})
+
+app.get("/*", (res, _req) => {
+    res.writeStatus("200 OK")
+        // .writeHeader("IsExample", "Yes")
+        .end("WebSocket Connections Only")
+})
+
+app.listen(PORT, (listenSocket) => {
+    if (listenSocket) {
+        console.log(`Listening to port ${PORT}`)
+    }
+})
