d86fce439237458335625d0e0bbef8ec9c18130f
Andrew <github@seventh.space>
2023-12-22 00:33:33 +0000

    chore: user WebSocketProvider and display connection status

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+// import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import "./index.css"
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
-import { RouterProvider } from "react-router-dom"
-import DataProvider from "./data/DataProvider"
-import { router } from "./router"
+// import { RouterProvider } from "react-router-dom"
+// import DataProvider from "./data/DataProvider"
+// import { router } from "./router"
 
-const theme = createTheme()
+// const theme = createTheme()
+
+import { WebSocketProvider } from "./WebSocketContext"
+import { useWebSocket } from "./useWebSocket"
+
+const ConnectionStatus = () => {
+    const { socket } = useWebSocket()
+
+    return socket?.readyState === WebSocket.OPEN ? "Connected" : "Not Connected"
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
+        <WebSocketProvider>
+            <ConnectionStatus />
+            {/* <ThemeProvider theme={theme}>
             <CssBaseline />
             <DataProvider>
                 <RouterProvider router={router} />
             </DataProvider>
-        </ThemeProvider>
+        </ThemeProvider> */}
+        </WebSocketProvider>
     </React.StrictMode>
 )
