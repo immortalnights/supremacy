29808ed92dbd0df934fb469fd30c64dade0359d7
Andrew <github@seventh.space>
2024-05-27 23:33:07 +0100

    chore: extend Planet and Ship types

--- src/Game/entities.ts
+++ src/Game/entities.ts
@@ -2,12 +2,25 @@ export interface Planet {
     id: string | number
     name: string
     owner?: string
+    credits: number
+    food: number
+    minerals: number
+    fuels: number
+    energy: number
+    population: number
+    growth: number
+    moral: number
+    tax: number
+    strength: number
 }
 
 export interface Ship {
     id: string
     name: string
     owner: string
+    class: ""
+    crew: number
+    fuel: number
     location: {
         planet?: Planet["id"]
         position: "landed" | "docked" | "orbit" | "outer-space"
