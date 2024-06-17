134ff2df1c8fcf8e9bd7acdd428a5854428b7d51
Andrew <github@seventh.space>
2024-06-17 23:38:47 +0100

    chore: add dev restart link

--- src/GameRoot.tsx
+++ src/GameRoot.tsx
@@ -1,11 +1,14 @@
 import { Provider } from "jotai"
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import { store } from "./Game/store"
 
 export default function InGame() {
     return (
         <Provider store={store}>
             <Outlet />
+            <div>
+                <Link to={"/Create"}>Restart</Link>
+            </div>
         </Provider>
     )
 }
