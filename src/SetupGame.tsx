fc9b93384aa7b9e51280dacb3af61c5d7acc7bf6
Andrew <github@seventh.space>
2024-06-17 23:39:08 +0100

    chore: initialize capital planets

--- src/SetupGame.tsx
+++ src/SetupGame.tsx
@@ -20,9 +20,44 @@ export default function SetupGame() {
         { length: planetsForDifficulty[settings.difficulty] },
         (_, index) => ({
             id: String(index),
-            name: "Unnamed",
+            name: "",
+            gridIndex: index,
         }),
     )
+
+    // EnemyBase (single player)
+    defaultPlanets[0] = {
+        ...defaultPlanets[0],
+        name: "EnemyBase",
+        owner: "cpu",
+        capital: true,
+        credits: 1000,
+        food: 1000,
+        minerals: 1000,
+        fuels: 1000,
+        energy: 1000,
+        population: 1000,
+        growth: 0,
+        moral: 0,
+        tax: 10,
+    }
+
+    const lastPlanet = defaultPlanets[defaultPlanets.length - 1]
+    defaultPlanets[defaultPlanets.length - 1] = {
+        ...lastPlanet,
+        name: "Homebase!",
+        owner: "local",
+        capital: true,
+        credits: 1000,
+        food: 1000,
+        minerals: 1000,
+        fuels: 1000,
+        energy: 1000,
+        population: 1000,
+        growth: 0,
+        moral: 0,
+        tax: 10,
+    }
 
     store.set(planetsAtom, defaultPlanets)
     store.set(fleetsAtom, [])
