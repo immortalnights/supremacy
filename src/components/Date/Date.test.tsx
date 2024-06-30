b92396e8e08f857ebb78500ab9a948503e030857
Andrew <github@seventh.space>
2024-06-30 13:25:22 +0100

    chore: add tests for the Date component

---
+++ src/components/Date/Date.test.tsx
@@ -0,0 +1,48 @@
+import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { Provider } from "jotai"
+import Date from "."
+import { dateAtom } from "../../Game/store"
+import { useHydrateAtoms } from "jotai/utils"
+import { ReactNode } from "react"
+
+const HydrateAtoms = ({
+    initialValues,
+    children,
+}: {
+    initialValues: Parameters<typeof useHydrateAtoms>[0]
+    children: ReactNode
+}) => {
+    useHydrateAtoms(initialValues)
+    return children
+}
+
+const TestProvider = ({
+    initialValues,
+    children,
+}: {
+    initialValues: Parameters<typeof useHydrateAtoms>[0]
+    children: ReactNode
+}) => (
+    <Provider>
+        <HydrateAtoms initialValues={initialValues}>{children}</HydrateAtoms>
+    </Provider>
+)
+
+const DateProvider = ({ value = 0 }: { value?: number }) => {
+    return (
+        <TestProvider initialValues={[[dateAtom, value]]}>
+            <Date />
+        </TestProvider>
+    )
+}
+
+test("render basic date", () => {
+    render(<DateProvider />)
+    expect(screen.getByLabelText("Date").textContent).toEqual("1/2000")
+})
+
+test("render basic date", () => {
+    render(<DateProvider value={200} />)
+    expect(screen.getByLabelText("Date").textContent).toEqual("21/2003")
+})
