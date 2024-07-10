89c74f1c14416fca40fc6d98f3ff9ddbb514108f
Andrew <github@seventh.space>
2024-07-11 00:18:22 +0100

    feat: allow a ship to be purchased

--- src/Game/Shipyard/Shipyard.tsx
+++ src/Game/Shipyard/Shipyard.tsx
@@ -1,69 +1,106 @@
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import Button from "../../components/Button"
-import { Planet, Ship } from "../entities"
+import { ColonizedPlanet, Planet, Ship } from "../entities"
 import battleship from "/images/battleship.gif"
 import solar from "/images/solar.gif"
+import catalog from "../../data/ships.json"
+import { selectedPlanetAtom } from "../store"
+import { useAtomValue } from "jotai"
+import { usePurchaseShip } from "../../commands"
 
-const catalog = [
-    {
-        id: "battleship",
-        img: battleship,
-        class: "battle",
-        cost: {
-            credits: 5250,
-            minerals: 95,
-            energy: 365,
-        },
-        requiredCrew: 0,
-        cargoCapacity: 0,
-        fuelCapacity: 0,
-        range: 0,
-        civilianCapacity: 0,
-    },
-    {
-        id: "solar",
-        img: solar,
-        class: "solar",
-        cost: {
-            credits: 975,
-            minerals: 7,
-            energy: 92,
-        },
-        requiredCrew: undefined,
-        cargoCapacity: 0,
-        fuelCapacity: Infinity,
-        range: Infinity,
-        civilianCapacity: 0,
-    },
-]
+const shipTypes: string[] = Object.keys(catalog)
 
-export default function Shipyard() {
-    const planet = {
-        credits: 0,
-        minerals: 0,
-        energy: 0,
-    } satisfies Planet
-    const [index, setIndex] = useState(0)
-    const ship = catalog[index]
-    const owned = 0
+const images: { [key: (typeof shipTypes)[number]]: string } = {
+    battlecruiser: battleship,
+    generator: solar,
+    atmos: battleship,
+    carrier: battleship,
+    miningstation: battleship,
+    horticultural: battleship,
+}
 
-    const handlePrevious = () => {
-        setIndex((index - 1 + catalog.length) % catalog.length)
-    }
-    const handleNext = () => {
-        setIndex((index + 1) % catalog.length)
+function PurchaseShip({
+    type,
+    planet,
+    owned,
+    onPurchased,
+    onCancel,
+}: {
+    type: string
+    planet: ColonizedPlanet
+    owned: number
+    onPurchased: () => void
+    onCancel: () => void
+}) {
+    const purchase = usePurchaseShip()
+    const [name, setName] = useState(`${type}${owned}`)
+
+    const handleChange = () => {}
+
+    const handleBuy = (ev: FormEvent) => {
+        ev.preventDefault()
+        purchase(type, planet, name)
+        onPurchased()
     }
 
     return (
         <div>
-            <img src={ship.img} />
+            <form onSubmit={handleBuy}>
+                <input type="text" value={name} onChange={handleChange} />
+                <Button type="submit">Buy</Button>
+                <Button type="button" onClick={onCancel}>
+                    Cancel
+                </Button>
+            </form>
+        </div>
+    )
+}
+
+export default function Shipyard() {
+    const planet = useAtomValue(selectedPlanetAtom) as ColonizedPlanet
+    const [index, setIndex] = useState(0)
+    const ship = catalog[shipTypes[index]]
+    const [purchasing, setPurchasing] = useState(false)
+    const owned = 0
+
+    const handlePrevious = () => {
+        setIndex((index - 1 + shipTypes.length) % shipTypes.length)
+    }
+    const handleNext = () => {
+        setIndex((index + 1) % shipTypes.length)
+    }
+    const handleBuy = () => {
+        setPurchasing(true)
+    }
+    const handlePurchased = () => {
+        setPurchasing(false)
+    }
+    const handleCancel = () => {
+        setPurchasing(false)
+    }
+
+    return (
+        <div>
+            <img src={images[shipTypes[index]]} />
             <div>
                 <div style={{ display: "flex" }}>
                     <Button onClick={handlePrevious} style={{ padding: 5 }}>
                         &lt;&lt;
                     </Button>
-                    <Button style={{ padding: 5 }}>Buy</Button>
-                    <div style={{ flexGrow: 1 }}></div>
+                    <Button style={{ padding: 5 }} onClick={handleBuy}>
+                        Buy
+                    </Button>
+                    <div style={{ flexGrow: 1 }}>
+                        {purchasing && (
+                            <PurchaseShip
+                                type={shipTypes[index]}
+                                planet={planet}
+                                owned={owned}
+                                onPurchased={handlePurchased}
+                                onCancel={handleCancel}
+                            />
+                        )}
+                    </div>
                     <Button onClick={handleNext} style={{ padding: 5 }}>
                         &gt;&gt;
                     </Button>
