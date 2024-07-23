1a49c4a09f35cc4233e3fc38c504b92f13baf35b
Andrew <github@seventh.space>
2024-07-23 01:35:49 +0100

    fix: set more data when commissioning a ship

--- src/Game/Shipyard/utilities.ts
+++ src/Game/Shipyard/utilities.ts
@@ -1,37 +1,62 @@
 import { ColonizedPlanet, Ship } from "../entities"
+import { shipInLocation } from "../utilities"
 
-export const canAffordShip = (planet: ColonizedPlanet, type: string) => {
+export const canAffordShip = (
+    planet: ColonizedPlanet,
+    type: string,
+): boolean => {
     return true
 }
 
-export const deductShipCost = (planet: ColonizedPlanet, type: string) => {
+export const deductShipCost = (
+    planet: ColonizedPlanet,
+    type: string,
+): ColonizedPlanet => {
     return { ...planet }
+}
+
+/**
+ * @param ships
+ * @param maxIndex
+ * @returns -1 or 1, 2, 3
+ */
+export const nextFreeIndex = (ships: Ship[], maxIndex: number): number => {
+    let availableIndex = -1
+    for (let index = 0; index < maxIndex; index++) {
+        const ship = shipInLocation(ships, index)
+        if (!ship) {
+            availableIndex = 1 + index
+            break
+        }
+    }
+
+    return availableIndex
 }
 
 export const commissionShip = (
     type: string,
     name: string,
-    owner: string,
+    planet: ColonizedPlanet,
+    index: number,
 ): Ship => {
     return {
         id: crypto.randomUUID(),
         name,
-        owner,
+        owner: planet.owner,
         class: type,
         // requiredCrew: number
-        // crew: number
+        crew: 0,
         // maxFuel: number
-        // fuel: number
-        // passengers: number
+        fuel: 0,
+        passengers: 0,
         // passengerLimit: number
         // cargoCapacity: number
-        // location: {
-        //     planet?: Planet["id"]
-        //     position: "landed" | "docked" | "orbit" | "outer-space"
-        //     // Bay or Planet Surface numeric location
-        //     index?: number
-        // }
-        // active: boolean
+        location: {
+            planet: planet.id,
+            position: "docked",
+            index,
+        },
+        active: false,
         // value: number
     }
 }
