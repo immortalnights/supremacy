681d05567b10c63f33f6832152c9c77d56e6bf56
Andrew <github@seventh.space>
2024-07-23 01:36:30 +0100

    feat: display selected ship heading

--- src/components/ShipHeading/ShipHeading.tsx
+++ src/components/ShipHeading/ShipHeading.tsx
@@ -1,15 +1,26 @@
+import { Ship } from "../../Game/entities"
 import Metadata from "../Metadata"
 
-export default function ShipHeading() {
+export default function ShipHeading({ ship }: { ship?: Ship }) {
+    let name = undefined
+    let from = undefined
+    let to = undefined
+    let eta = undefined
+    let fuel = undefined
+    if (ship && ship.location.position === "outer-space") {
+        ;({ name, fuel } = ship)
+        ;({ from, to, eta } = ship.location.heading)
+    }
+
     return (
         <div>
             Heading
             <div>
-                <Metadata label="Ship" />
-                <Metadata label="From" />
-                <Metadata label="To" />
-                <Metadata label="ETA" />
-                <Metadata label="Fuel" />
+                <Metadata label="Ship" value={name} />
+                <Metadata label="From" value={from} />
+                <Metadata label="To" value={to} />
+                <Metadata label="ETA" value={eta} />
+                <Metadata label="Fuel" value={fuel} />
             </div>
         </div>
     )
