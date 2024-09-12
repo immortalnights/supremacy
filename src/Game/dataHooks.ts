c4f70fc1524aeb7572779ba2898289c6e3c1e408
Andrew <github@seventh.space>
2024-09-12 23:30:37 +0100

    chore: add data hook for selected planet and capital planet

--- src/Game/dataHooks.ts
+++ src/Game/dataHooks.ts
@@ -1,7 +1,39 @@
 import { useAtomValue } from "jotai"
-import { shipsAtom } from "./store"
+import {
+    planetsAtom,
+    selectedPlanetAtom,
+    sessionAtom,
+    shipsAtom,
+} from "./store"
 import { shipsDockedAtPlanet, shipsOnPlanetSurface } from "./utilities"
-import type { Planet } from "./entities"
+import type { ColonizedPlanet, Planet } from "./entities"
+
+const isColonizedPlanet = (planet: Planet): planet is ColonizedPlanet =>
+    planet.type !== "lifeless"
+
+export const useSelectedPlanet = () => {
+    const planet = useAtomValue(selectedPlanetAtom)
+    return planet
+}
+
+export const useSelectedColonizedPlanet = () => {
+    const planet = useSelectedPlanet()
+    return planet && isColonizedPlanet(planet) ? planet : undefined
+}
+
+export const useCapitalPlanet = () => {
+    const { localPlayer } = useAtomValue(sessionAtom)
+    const planets = useAtomValue(planetsAtom)
+
+    const capital = planets.find(
+        (planet) =>
+            isColonizedPlanet(planet) &&
+            planet.capital &&
+            planet.owner === localPlayer,
+    )
+
+    return capital && isColonizedPlanet(capital) ? capital : undefined
+}
 
 export const useShipsInDockingBay = (planet: Planet) => {
     const ships = useAtomValue(shipsAtom)
