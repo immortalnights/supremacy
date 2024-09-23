203f31562afa558dcf704caa92d860b6d9e87b07
Andrew <github@seventh.space>
2024-09-23 23:25:45 +0100

    chore: configuration

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    semi: false,
+    tabWidth: 4,
+    trailingComma: "all",
+    singleQuote: false,
+}
+
+export default config
