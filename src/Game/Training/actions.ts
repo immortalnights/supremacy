b300d0c9cb4cc24717a3077e95d6b808191f0c7e
Andrew <github@seventh.space>
2024-10-06 23:03:25 +0100

    chore: add platoon equipment actions

--- src/Game/Training/actions.ts
+++ src/Game/Training/actions.ts
@@ -1,11 +1,23 @@
 import { useContext } from "react"
 import { CommandContext } from "../CommandContext"
-import { Platoon } from "../entities"
+import { Platoon, SuitClass, WeaponClass } from "../entities"
 
 export const useModifyPlatoonTroops = () => {
     const { exec } = useContext(CommandContext)
     return (platoon: Platoon, quantity: number) =>
         exec("modify-platoon-troops", { platoon, quantity })
+}
+
+export const useModifyPlatoonSuit = () => {
+    const { exec } = useContext(CommandContext)
+    return (platoon: Platoon, suit: SuitClass) =>
+        exec("modify-platoon-suit", { platoon, suit })
+}
+
+export const useModifyPlatoonWeapon = () => {
+    const { exec } = useContext(CommandContext)
+    return (platoon: Platoon, weapon: WeaponClass) =>
+        exec("modify-platoon-weapon", { platoon, weapon })
 }
 
 export const useEquipPlatoon = () => {
@@ -16,4 +28,14 @@ export const useEquipPlatoon = () => {
 export const useDismissPlatoon = () => {
     const { exec } = useContext(CommandContext)
     return (platoon: Platoon) => exec("dismiss-platoon", { platoon })
+}
+
+export const useTrainingActions = () => {
+    const modifyTroops = useModifyPlatoonTroops()
+    const modifySuit = useModifyPlatoonSuit()
+    const modifyWeapon = useModifyPlatoonWeapon()
+    const equip = useEquipPlatoon()
+    const dismiss = useDismissPlatoon()
+
+    return { modifyTroops, modifySuit, modifyWeapon, equip, dismiss }
 }
