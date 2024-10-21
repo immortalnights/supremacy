b03f7695915000f74cd6e56aa77b683357e0cd88
Andrew <github@seventh.space>
2024-10-21 22:40:17 +0100

    chore: add selected ship hook

--- src/Game/hooks/ships.ts
+++ src/Game/hooks/ships.ts
@@ -1,7 +1,26 @@
 import { useAtomValue } from "jotai"
 import { shipsAtom } from "../store"
-import type { Planet } from "../entities"
+import type { Planet, Ship } from "../entities"
 import { shipsDockedAtPlanet, shipsOnPlanetSurface } from "Game/utilities/ships"
+import { useCallback, useState } from "react"
+
+export const useSelectedShip = () => {
+    const [selectedShip, setSelectedShip] = useState<Ship["id"] | undefined>()
+    const ships = useAtomValue(shipsAtom)
+    let ship
+    if (selectedShip) {
+        ship = ships.find((item) => item.id === selectedShip)
+    }
+
+    const setShip = useCallback(
+        (ship: Ship) => {
+            setSelectedShip(ship.id)
+        },
+        [setSelectedShip],
+    )
+
+    return [ship, setShip] as const
+}
 
 // replace with platoonsOnPlanetAtom like function
 export const useShipsInDockingBay = (planet: Planet) => {
