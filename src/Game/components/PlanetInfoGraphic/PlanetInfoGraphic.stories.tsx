cabdad386b853b82a7a306a23080d8a5b15b2096
Andrew <github@seventh.space>
2024-10-31 22:57:08 +0000

    chore: fix PlanetInfoGraphic story

--- src/Game/components/PlanetInfoGraphic/PlanetInfoGraphic.stories.tsx
+++ src/Game/components/PlanetInfoGraphic/PlanetInfoGraphic.stories.tsx
@@ -13,4 +13,13 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+    args: {
+        planet: {
+            id: "1",
+            name: "",
+            type: "lifeless",
+            gridIndex: 0,
+        },
+    },
+}
