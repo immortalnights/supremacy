7a60b07d2ee32f2633ced09061a8417b62f150f6
Andrew <github@seventh.space>
2024-10-31 22:57:41 +0000

    chore: add utility to get player capital

--- src/Game/utilities/planets.ts
+++ src/Game/utilities/planets.ts
@@ -1,4 +1,16 @@
 import { ColonizedPlanet, Planet } from "Game/entities"
 
+export const getPlayerCapital = (planets: Planet[], player: string) => {
+    const index = planets.findIndex(
+        (p) => p.type !== "lifeless" && p.capital && p.owner === player,
+    )
+
+    if (index === -1) {
+        throw new Error(`Failed to find capital planet for player ${player}`)
+    }
+
+    return [index, planets[index] as ColonizedPlanet] as const
+}
+
 export const isColonizedPlanet = (planet: Planet): planet is ColonizedPlanet =>
     planet.type !== "lifeless"
