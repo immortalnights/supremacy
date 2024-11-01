6979a854baed3c22d990fff5272c6368699a0074
Andrew <github@seventh.space>
2024-11-01 02:16:46 +0000

    fix: handle ship details better

--- src/Game/Fleet/components/ShipDetails/ShipDetails.tsx
+++ src/Game/Fleet/components/ShipDetails/ShipDetails.tsx
@@ -1,39 +1,31 @@
 import { Ship } from "Game/entities"
 import Date from "Game/components/Date"
-import Metadata, {
-    MetadataLabel,
-    MetadataValue,
-} from "Game/components/Metadata"
+import Metadata, { MetadataLabel, MetadataValue } from "Game/components/Metadata"
 
 export default function ShipDetails({ ship }: { ship?: Ship }) {
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
             <div style={{ display: "flex", gap: 6 }}>
-                <div
-                    style={{ display: "flex", flexDirection: "column", gap: 6 }}
-                >
-                    <Metadata
-                        label="Ship"
-                        value={ship?.name}
-                        alignment="right"
-                    />
+                <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+                    <Metadata label="Ship" value={ship?.name} alignment="right" />
                     <Date alignment="right" />
                 </div>
-                <div
-                    style={{ display: "flex", flexDirection: "column", gap: 6 }}
-                >
-                    <Metadata label="Crew" value={ship?.crew} />
-                    <Metadata label="Fuel" value={ship?.fuels} postfix="T." />
+                <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+                    <Metadata
+                        label="Crew"
+                        value={ship?.requiredCrew === "remote" ? "Remote" : ship?.crew}
+                    />
+                    <Metadata
+                        label="Fuel"
+                        value={ship?.fuels}
+                        postfix={ship?.fuels !== "nuclear" ? "T." : ""}
+                    />
                 </div>
             </div>
 
             <div style={{ display: "flex", gap: 6 }}>
                 <MetadataLabel label="Class" textAlign="right" />
-                <MetadataValue
-                    label="Class"
-                    value={ship?.class}
-                    textAlign="center"
-                />
+                <MetadataValue label="Class" value={ship?.class} textAlign="center" />
                 <div
                     style={{
                         width: 80,
